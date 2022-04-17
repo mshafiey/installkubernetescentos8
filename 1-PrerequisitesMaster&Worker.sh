5d12e302ba87ecd9a6791e8d90234c84dc406e9e
Mohsen Shafiey Kasrineh <mshafiey@users.noreply.github.com>
2022-04-17 10:28:06 +0300

    "\n

--- 1-PrerequisitesMaster&Worker.sh
+++ 1-PrerequisitesMaster&Worker.sh
@@ -1,11 +1,11 @@
 echo "ervers are up to date before installing "
 dnf -y upgrade
-"echo ---------------------------------------------------------------done"
+"echo \n---------------------------------------------------------------done"
 
 echo "Disable SELinux enforcement."
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-echo ---------------------------------------------------------------done
+echo "\n---------------------------------------------------------------done"
 
 echo "Enable transparent masquerading and facilitate Virtual Extensible LAN (VxLAN) "
 modprobe br_netfilter
@@ -14,7 +14,7 @@ echo "---------------------------------------------------------------done"
 echo "You will also need to enable IP masquerade at the firewall."
 firewall-cmd --add-masquerade --permanent
 firewall-cmd --reload
-"echo ---------------------------------------------------------------done"
+echo "\n---------------------------------------------------------------done"
 
 
 echo "Set bridged packets to traverse iptables rules."
@@ -22,17 +22,17 @@ cat < /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-"echo ---------------------------------------------------------------done"
+echo "\n---------------------------------------------------------------done"
 
 
 echo "Then load the new rules."
 sysctl --system
-"echo ---------------------------------------------------------------done"
+echo "\n---------------------------------------------------------------done"
 
 
 echo "Disable all memory swaps to increase performance."
 swapoff -a
-"echo ---------------------------------------------------------------done"
+echo "\n---------------------------------------------------------------done"
 
 
 
